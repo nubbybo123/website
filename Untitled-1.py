cc78a51dd80037960cec384e91a1dd4f34b3884d
kavish mahaseth <mahasethkavish@gmail.com>
2025-11-11 17:30:32 -0500

    Add files via upload
    12342634242964126341284126842187417235871345092365814

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,23 @@
-print("hello")
+import random
+
+alist = ["rock","paper","scissors"]
+computer_action = random.choice(alist)
+user_action = input("Enter rock, paper, or scissors. cmon i dont have all day!")
+
+if user_action == computer_action:
+    print("dude how did u tie?!")
+if user_action == "scissors":
+    if computer_action == "paper":
+        print("congrats i guess, you won")
+    else:
+        print("cmon man how did you lose to a bot! do better!")
+elif user_action == "rock":
+    if computer_action == "scissors":
+        print("congrats i guess, you won")
+    else:
+        print("cmon man how did you lose to a bot! do better!")
+if user_action == "paper":
+    if computer_action == "rock":
+        print("congrats i guess, you won")
+    else:
+        print("cmon man how did you lose to a bot! do better")
